8ad465b6541b8be65992de18b8097a4d291f96d1
salman3692 <m.salman@uliege.be>
2025-08-15 11:36:48 +0200

    modified:   Cement/pareto-frontend/src/App.jsx

--- Cement/pareto-frontend/src/App.jsx
+++ Cement/pareto-frontend/src/App.jsx
@@ -56,7 +56,9 @@ export default function App() {
         ),
         emission_scenario: emissionScenario
       };
-      const res = await fetch(`${API_BASE}/predict`, {
+      // const res = await fetch(`${API_BASE}/predict`, {
+      const res = await fetch('http://127.0.0.1:8000/predict', {
+
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(payload),
